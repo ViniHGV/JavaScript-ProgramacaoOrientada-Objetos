d9af2702e813c4c5f224311a39e9c3ad5c7d10ee
Vini <ixxvinicius@gmail.com>
2023-01-17 22:16:25 -0300

    Adicionando método Login na classe user

---
+++ Treinando-Criação-Classes/Product.js
@@ -0,0 +1,11 @@
+class Product{
+    constructor(){
+        this.name = name
+        this.description = description
+        this.price = price
+        this.inStock = 0
+    }
+    addToStock = function(quantity){
+        inStock += quantity
+    }
+}
