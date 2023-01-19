a0b1ed3b72a7a3816e309165b3defdd01329c8b7
Vini <ixxvinicius@gmail.com>
2023-01-19 16:56:49 -0300

    Associação

--- Associação/index.js
+++ Associação/index.js
@@ -1,8 +1,8 @@
 const Address = require("./Address");
 const Person = require("./Person");
 
-const addr = new Address("7 de setembro",99, "São Paulo", "SP")
-const Vns = new Person("Vns", addr)
+//const addr = new Address("7 de setembro",99, "São Paulo", "SP")
+const Vns = new Person("Vns","7 de setembro",99, "São Paulo", "SP")
 
 console.log(Vns)
-console.log(Vns.Address.fullAddress())
+console.log(Vns.adrres.fullAddrees())
