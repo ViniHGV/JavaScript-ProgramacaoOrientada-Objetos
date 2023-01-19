908bd288e19d74b417b5df59ed89bdab7ba45a91
Vini <ixxvinicius@gmail.com>
2023-01-19 17:18:30 -0300

    Polimorfismo

--- Polimorfismo/index.js
+++ Polimorfismo/index.js
@@ -1,6 +1,23 @@
 class veiculo{
     mover(){
-        console.log("O Veiculo esta se movendo")
+        console.log("O Veiculo está se movendo")
     }
 }
 
+class carro extends veiculo{
+    mover(){
+        console.log("O Carro está se movendo")
+    }
+}
+
+class bike extends veiculo{
+    mover(){
+        console.log("A bike está se movendo")
+    }
+}
+
+const Carro = new carro()
+const Bike = new bike()
+
+Carro.mover()
+Bike.mover()
